968d2e69ac4cee0bc90f4942e68a7f8f0d63a269
longfei Zhou <longfei Zhou>
2016-12-31 21:36:55 +0800

    创建

---
+++ pachong/jian_dan.py
@@ -0,0 +1,15 @@
+from bs4 import BeautifulSoup
+import requests
+def gettext(url):
+    html=requests.get(url).text
+    soup=BeautifulSoup(html,'lxml')
+    return soup.find('div',{'class':'post f'}).get_text()
+html=requests.get('http://jandan.net/').text
+bsobj=BeautifulSoup(html,'lxml')
+total=bsobj.find_all('div',{'class':'post f list-post'})
+text=open('c://users/longfei/desktop/utl.txt','w')
+for single in total:
+    a=single.h2.a
+    text.write(gettext(str(a['href'])))
+text.close()
+
