90b6cd5b55458b2dfd83cc8cf0a89a7c2db91df8
longfei Zhou <longfei Zhou>
2017-01-02 07:48:34 +0800

    zhengliban

--- pachong/jian_dan.py
+++ pachong/jian_dan.py
@@ -1,20 +1,17 @@
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
+import random
+import time
 
 
-def gettext(url):
-    html = requests.get(url).text
-    soup = BeautifulSoup(html, 'lxml')
-    return soup.find('div', {'class': 'post f'}).get_text()
+def GetUrl():
+    url = 'http://jandan.net/ooxx/page-' + str(random.randrange(0, 2299))
+    return url
 
 
-html = requests.get('http://jandan.net/').text
-bsobj = BeautifulSoup(html, 'lxml')
-total = bsobj.find_all('div', {'class': 'post f list-post'})
-text = open('c://users/longfei/desktop/utl.txt', 'w')
-for single in total:
-    a = single.h2.a
-    b = gettext(str(a['href']))
-    text.write(b)
-    print(b)
-text.close()
+url = GetUrl()
+print(url)
+web_date = requests.get(url).text
+print(web_date)
+
+input("wait")
