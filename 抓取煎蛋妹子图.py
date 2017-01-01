7ee829a384ab59f1af12dfe5747dbe7d434750d4
longfei Zhou <longfei Zhou>
2017-01-02 07:50:57 +0800

    整理

--- 抓取煎蛋妹子图.py
+++ 抓取煎蛋妹子图.py
@@ -3,13 +3,15 @@ from bs4 import BeautifulSoup
 import time
 import os
 
-cur_page=1
-picture=0
-urls = ['http://jandan.net/ooxx/page-{}'.format(str(i))for i in range(800,2290)]
+cur_page = 1
+picture = 0
+urls = [
+    'http://jandan.net/ooxx/page-{}'.format(str(i))for i in range(800, 2290)]
+
 
 def getImage(ads):
     global picture
-    picture+=1
+    picture += 1
     '''print('Downloading...')
     name = 1;
     for imgurl in imList:
@@ -19,29 +21,30 @@ def getImage(ads):
     '''
     print('request img...')
     print('request succeed!')
-    try :
+    try:
         ir = requests.get(ads)
-        path = os.path.dirname(__file__) +'/img/' +str(picture)+'.jpg'# 获取当前文件夹的绝对路
+        path = os.path.dirname(__file__) + '/img/' + \
+            str(picture) + '.jpg'  # 获取当前文件夹的绝对路
         print(path)
         print('Downloading ...')
         open(path, 'wb').write(ir.content)
-        print('picture',picture,' download succsed!')
+        print('picture', picture, ' download succsed!')
     except:
-        print('picture ',picture,' download failed!'  )
+        print('picture ', picture, ' download failed!')
+
 
 for url in urls:
-    print('-----------------page',cur_page,'in 2290','-----------------')
+    print('-----------------page', cur_page, 'in 2290', '-----------------')
     web_date = requests.get(url)
     time.sleep(2)
-    soup=BeautifulSoup(web_date.text,"lxml")
-    ImgUrl=soup.select('div.text > p > img')
+    soup = BeautifulSoup(web_date.text, "lxml")
+    ImgUrl = soup.select('div.text > p > img')
     for i in ImgUrl:
-        ads=str(i.get('src'))
+        ads = str(i.get('src'))
         if ads.find('http:') == 0:
             pass
         else:
-            ads='http:'+ads
-        print (ads)
+            ads = 'http:' + ads
+        print(ads)
         getImage(ads)
-    cur_page+=1
-
+    cur_page += 1
